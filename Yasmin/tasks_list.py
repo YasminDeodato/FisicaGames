8ed6781240647a1b5a3cb17838a4b63cc0e5c35d
Yasmin Deodato <50773119+YasminDeodato@users.noreply.github.com>
2019-09-07 23:17:40 -0300

    Lista de Tarefas
    Manipulação de listas
    Funções: adicionar, listar, remover pelo índice e remover pelo nome.
    Disponível em: http://www.codeskulptor.org/#user46_wwkcQ1JTiQ_2.py

---
+++ Yasmin/tasks_list.py
@@ -0,0 +1,50 @@
+# Listas
+# criado por Yasmin Deodato
+# disponível em: http://www.codeskulptor.org/#user46_wwkcQ1JTiQ_2.py
+#######################################################################
+import simplegui
+
+# Handler for clear button
+def clearAll():
+    global tasks
+    tasks = []
+    
+# Handler for Task
+def newTask(task):
+    global tasks
+    tasks.append(task)
+
+def removeTask(input):
+    global tasks
+    
+    if(input.isdigit()):
+        #if input is a number
+        tasks.pop(int(input)-1)
+    else:
+        #if input is a task
+        if(input in tasks):
+            tasks.remove(input)
+    
+# Handler to write on canvas
+def draw(canvas):
+    global tasks, height, index
+    height = 40
+    
+    for index, item in enumerate(tasks):
+        index = str(index + 1) + ": "
+        canvas.draw_text(index + item, [20, height], 28, "White")
+        height += 30 
+
+# Create a frame and assign callbacks to event handlers
+frame = simplegui.create_frame("Listas", 500, 400)
+task = frame.add_input('Nova tarefa', newTask, 150)
+removeTaskNumber = frame.add_input('Remova a tarefa pelo numero', removeTask, 150)
+removeTaskName = frame.add_input('Remova a tarefa pelo nome', removeTask, 150)
+frame.add_button("Limpar tudo", clearAll)
+
+#reset list
+clearAll()
+frame.set_draw_handler(draw)
+
+# Start the frame animation
+frame.start()
