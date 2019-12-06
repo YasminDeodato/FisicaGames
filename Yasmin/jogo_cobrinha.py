1f0daf34cd046ff7d6abcf501f668fac12258188
Yasmin Deodato <50773119+YasminDeodato@users.noreply.github.com>
2019-12-06 20:39:21 -0300

    Atualização Jogo da Cobrinha

--- Yasmin/jogo_cobrinha.py
+++ Yasmin/jogo_cobrinha.py
@@ -1,183 +1,167 @@
 # Jogo da Cobrinha
 # Criado por Yasmin Deodato
-# Disponível em: http://www.codeskulptor.org/#user46_xTrdhFy2r3_1.py
+# Disponível em: http://www.codeskulptor.org/#user46_6T6NT98Pnh_4.py
 ####################################################################
 import simplegui
 import random
-import math
 
 # inicializa as variáveis globais
 LARGURA = 600
-ALTURA = 400
-
-COMIDA_RAIO = 10
-comida_pos = [0, 0]
-
-BOLA_RAIO = 12
-
-cabeca = [LARGURA/2, ALTURA/2]
-corpo = []
-
-jogo = True
-mensagem = ''
-pontuacao = 0
-
-const = 5
-vel = [0, 4]
+ALTURA = 600
 
 # Handler p/ os botões 
 def iniciar():
-    global comida_pos, cabeca, corpo, jogo, mensagem, pontuacao, const, vel
+    global cabeca, cobra, vel, comida, jogo, mensagem, pontuacao
     
-    comida_pos = [LARGURA/2, ALTURA/4]
+    timer_reiniciar.stop()
+    timer.start()
     
-    cabeca = [LARGURA/2, ALTURA/2]
-    corpo = []
- 
+    cabeca = [140, 60]
+    cobra = [[cabeca[0] - 80, cabeca[1]],[cabeca[0] - 40 ,cabeca[1]], cabeca]
+    vel = [1, 0]
+    posicaoComida()
     jogo = True
-    mensagem = ''
+    mensagem = ' '
     pontuacao = 0
-    
-    const = 5
-    vel = [0, 0]
     
 def pausar():
     global jogo
-    if jogo == True:
+    if jogo:
         jogo = False 
     else:
         jogo = True
+
+# Handler do timer
+def timer_handler():
+    global cabeca, vel, comida, cobra, pontuacao, jogo, mensagem, timer_reiniciar
     
-# Posição aleatória da comida    
-def comidaPosicao():
-    global comida_pos
-    comida_pos[0] = random.randrange(BOLA_RAIO, LARGURA - BOLA_RAIO)
-    comida_pos[1] = random.randrange(BOLA_RAIO, ALTURA - BOLA_RAIO)
-    
-# Draw handler 
-def draw(canvas):
-    global cabeca, corpo, cobra, comida_pos, pontuacao, mensagem, vel
-         
-    if jogo == True:
-        # verifica se há colisão
+    if jogo:
+        if cabeca[0] == comida[0] and cabeca[1] == comida[1]:
+            # adiciona um novo gomo no corpo da cobra
+            cobra.append(list(comida))
+            
+            # atualiza a posição da comida
+            posicaoComida()
+        
+            # atualiza a pontuação
+            pontuacao += 10
+        else:
+            # atualiza a posição da cabeca
+            cabeca[0] += 40*vel[0]
+            cabeca[1] += 40*vel[1]
+            
+            cobra.append(list(cabeca))
+            cobra.pop(0)
+       
+        # verifica se há colisão da cobra
         colisao()
+     
+    if jogo == False and mensagem == 'Fim de Jogo.':
+        timer_reiniciar.start()
         
-        # verifica se a cobra comeu a comida
-        comeuComida()
-               
-        # atualiza posição dos gomos do corpo
-        if corpo: 
-            # move os gomos em ordem reversa
-            for index in range(len(corpo) - 1, 0, -1):                        
-                        x = corpo[index - 1][0] - 2 * BOLA_RAIO * vel[0]
-                        y = corpo[index - 1][1] - 2 * BOLA_RAIO * vel[1]
-                        corpo[index] = [x, y]
-                        
-            # move o primeiro segmento do corpo p/ posicao da cabeca 
-            if len(corpo) > 0:
-                corpo[0] = [cabeca[0] - 2 * BOLA_RAIO * vel[0], cabeca[1] - 2 * BOLA_RAIO * vel[1]]
-        
-        # atualiza a posição da cabeça da cobra
-        mover()
-        
-    
-    # escreve a pontuação 
-    canvas.draw_text('Pontos: ' + str(pontuacao), (5, 20), 24, 'White')
-    
-    # escreve a mensagem 
-    canvas.draw_text(mensagem, (LARGURA/3, ALTURA/2), 24, 'Red')
-    
-    # desenha o corpo da cobra
-    if corpo:
-        for gomo in corpo:
-            canvas.draw_circle(gomo, BOLA_RAIO, 1, "White", "White")
-           
-    # desenha a cabeça da cobra
-    canvas.draw_circle(cabeca, BOLA_RAIO, 1, "Green", "Green")
-        
-    # desenha a comida
-    canvas.draw_circle(comida_pos, COMIDA_RAIO, 1, "red", "red")
-
-# Move a cabeça da cobra
-def mover():
-    global cabeca, corpo, vel   
-        
-    cabeca[0] += vel[0]
-    cabeca[1] += vel[1]
-    
-# Verifica se a cobra comeu a comida
-def comeuComida():
-    global cabeca, corpo, comida_pos, pontuacao, const
-    
-    if len(corpo) < 1:
-        gomo_anterior = cabeca
-    else: 
-        gomo_anterior = corpo[len(corpo) - 1]
-    
-    # calcula a distancia entre a cabeça da cobra e a comida
-    distancia = math.sqrt(((cabeca[0]-comida_pos[0])**2)+((cabeca[1]-comida_pos[1])**2))   
-    
-    # se a cobra tangencia a comida
-    if distancia <= BOLA_RAIO + COMIDA_RAIO:
-        # adiciona um novo gomo no corpo da cobra
-        corpo.append([gomo_anterior[0] + 2 * BOLA_RAIO, gomo_anterior[1] + 2 * BOLA_RAIO])
-
-        # atualiza a posição da comida
-        posicaoComida()
-        
-        # atualiza a pontuação
-        pontuacao += 10
-        
+def timer_reiniciar():
+    timer.stop()
+    iniciar()
     
 # Posição aleatória da comida    
 def posicaoComida():
-    global comida_pos
-    comida_pos[0] = random.randrange(COMIDA_RAIO, LARGURA - COMIDA_RAIO)
-    comida_pos[1] = random.randrange(COMIDA_RAIO, ALTURA - COMIDA_RAIO)
-
+    global comida, cobra
+    
+    nao_deu_certo = True
+    semente = [-1,-1]
+    
+    while nao_deu_certo:
+        semente[0] = random.randrange(60, 560, 40)
+        semente[1] = random.randrange(60, 560, 40)
+        
+        if semente not in cobra:
+            nao_deu_certo = False
+            comida = semente
+    
+# Draw handler 
+def draw(canvas):
+    global cobra, mensagem, pontuacao 
+    
+    # Desenha as linhas guias
+    '''for i in range(16):           
+        canvas.draw_line([0,0 + 40*i], [LARGURA, 0 + 40*i], 2, "White")
+        canvas.draw_line([0 + 40*i,0], [0 + 40*i, ALTURA], 2, "White")'''
+         
+    # Desenha corpo
+    if cobra:
+        for gomo in cobra:
+            #cabeça
+            if cobra.index(gomo) == len(cobra)-1:
+                canvas.draw_polygon([(gomo[0]-20, gomo[1]-20), 
+                                 (gomo[0]-20, gomo[1]+20), 
+                                 (gomo[0]+20, gomo[1]+20), 
+                                 (gomo[0]+20, gomo[1]-20)], 2, 'White', 'Red')
+            #corpo
+            else:
+                canvas.draw_polygon([(gomo[0]-20, gomo[1]-20), 
+                                     (gomo[0]-20, gomo[1]+20), 
+                                     (gomo[0]+20, gomo[1]+20), 
+                                     (gomo[0]+20, gomo[1]-20)], 2, 'White', 'White')
+        
+    # desenha a comida
+    canvas.draw_polygon([(comida[0]-20, comida[1]-20), 
+                         (comida[0]-20, comida[1]+20), 
+                         (comida[0]+20, comida[1]+20), 
+                         (comida[0]+20, comida[1]-20)], 2, 'White', 'Green')
+    
+    # escreve a pontuação 
+    canvas.draw_text('Pontos: ' + str(pontuacao), (5, 30), 30, 'White')
+    
+    # escreve a mensagem 
+    canvas.draw_text(mensagem, (LARGURA/3, ALTURA/2), 48, 'Red')
+    
 # Colisão    
 def colisao():
-    global cabeca, corpo, mensagem, jogo
+    global cobra, mensagem, jogo, cabeca
     
-    if corpo: 
-        ultimo_gomo = corpo[len(corpo)-1]
-    else:    #se o corpo estiver vazio, o ultimo_gomo será a cabeça
-        ultimo_gomo = cabeca
-        
-    primeiro_gomo = cabeca
+    ultimo_gomo = cobra[len(cobra)-1]
+    primeiro_gomo = cobra[0]
     
-    # colisão com as paredes
-    if primeiro_gomo[0] > LARGURA - (BOLA_RAIO + 1) or ultimo_gomo[0] > LARGURA - (BOLA_RAIO + 1):
+    #colisão com as paredes
+    if primeiro_gomo[0] + 40 > LARGURA or ultimo_gomo[0] + 40 > LARGURA:
         jogo = False
-    elif primeiro_gomo[0] < BOLA_RAIO + 1 or ultimo_gomo[0] < BOLA_RAIO + 1:
+    elif primeiro_gomo[0] - 40 < 0 or ultimo_gomo[0] - 40 < 0:
         jogo = False
-    elif primeiro_gomo[1] < BOLA_RAIO + 1 or ultimo_gomo[1] < BOLA_RAIO + 1:
+    elif primeiro_gomo[1] - 40 < 0 or ultimo_gomo[1] - 40 < 0:
         jogo = False
-    elif primeiro_gomo[1] > ALTURA - (BOLA_RAIO + 1) or ultimo_gomo[1] > ALTURA - (BOLA_RAIO + 1):
+    elif primeiro_gomo[1] + 40 > ALTURA or ultimo_gomo[1] + 40 > ALTURA:
         jogo = False
     
     # colisão com o próprio corpo
-    if primeiro_gomo[0] == ultimo_gomo[0] and primeiro_gomo[1] == ultimo_gomo[1] - BOLA_RAIO:
+    if primeiro_gomo[0] - 40 == ultimo_gomo[0] and primeiro_gomo[1] == ultimo_gomo[1] \
+        or primeiro_gomo[0] + 40 == ultimo_gomo[0] and primeiro_gomo[1] == ultimo_gomo[1]:
         jogo = False
     
     # atualiza mensagem
     if jogo == False:
-        mensagem = 'Fim de Jogo'
-
+        mensagem = 'Fim de Jogo.'
+        
 # Evento do teclado
 def keydown(key):
-    global vel, const
-    if key == simplegui.KEY_MAP["right"]:
-        vel = const * [1,0]
-    if key == simplegui.KEY_MAP["left"]:
-        vel = const * [-1,0]
-    if key == simplegui.KEY_MAP["up"]:
-        vel = const * [0,-1]
-    if key == simplegui.KEY_MAP["down"]:
-        vel = const * [0,1]
+    global vel, jogo
+    
+    if jogo:
+        if key == simplegui.KEY_MAP["right"]:
+            jogo = True
+            vel = [1,0]
+        if key == simplegui.KEY_MAP["left"]:
+            jogo = True
+            vel = [-1,0]
+        if key == simplegui.KEY_MAP["up"]:
+            jogo = True
+            vel = [0,-1]
+        if key == simplegui.KEY_MAP["down"]:
+            jogo = True
+            vel = [0,1]
     if key == simplegui.KEY_MAP["space"]:
         pausar()
-        
+        vel = [0,0]
+
 # Cria o frame
 frame = simplegui.create_frame("Jogo da Cobrinha", LARGURA, ALTURA)
 # Adiciona os componentes da tela
@@ -188,6 +172,11 @@ frame.add_button("Pausar Jogo", pausar)
 # Registra os handlers
 frame.set_draw_handler(draw)
 frame.set_keydown_handler(keydown)
+
+# Registra um timer
+timer = simplegui.create_timer(250, timer_handler)
+timer.start()
+timer_reiniciar = simplegui.create_timer(2000, timer_reiniciar)
 
 # Inicia o frame
 iniciar()
