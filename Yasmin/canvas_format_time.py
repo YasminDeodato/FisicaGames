eb6c24144f05575bccde9ae24648213c5fc0afcb
Yasmin Deodato <50773119+YasminDeodato@users.noreply.github.com>
2019-12-06 20:40:19 -0300

    Atualização - Formatador de Tempo

--- Yasmin/canvas_format_time.py
+++ Yasmin/canvas_format_time.py
@@ -1,26 +1,47 @@
-# Função que retorna o número formatado em minutos e segundos
+# FunÃ§Ã£o que retorna o nÃºmero formatado em minutos e segundos
 # criada por Yasmin Deodato
-# Disponível em: http://www.codeskulptor.org/#user46_AIzow1OQQd_4.py
+# DisponÃ­vel em: http://www.codeskulptor.org/#user46_AeovuqOjJl_8.py
 ####################################################################
 import random
+import simplegui
 
-# Função de formatação do tempo
-def format_time(number):
-    return "%s minutos e %s segundos"%(number/60, number)
-
+time = ''
+# FunÃ§Ã£o de formataÃ§Ã£o do tempo
+def format_time(input):
+    global time
+    input = int(input)
+    if input >= 3600:
+        time = "%sh %smin %ss"%(input/3600, input%3600/60, input%3600%60)
+    else:
+        time = "%smin %ss"%(input/60, input%60)
+    
+    print time
+    
 def random_number():
     return random.randrange(0, 3600)
+    
+# Handler to write on canvas
+def draw(canvas):
+    global time
+    canvas.draw_text(time, [50, 100], 28, "White")
+    
+# Cria um frame
+frame = simplegui.create_frame("Conversao de Segundos", 300, 200)
+frame.add_input("Digite o tempo em segundos e aperte enter: ", format_time, 100)
+
+frame.set_draw_handler(draw)
+frame.start()
 
 ###################################################
 # Testes
 
-print format_time(23)
+'''print format_time(23)
 print format_time(1237)
 print format_time(0)
-print format_time(1860)
+print format_time(1860)'''
 
 ###################################################
-# Saída no console
+# SaÃ­da no console
 #0 minutos e 23 segundos
 #20 minutos e 37 segundos
 #0 minutos e 0 segundos
